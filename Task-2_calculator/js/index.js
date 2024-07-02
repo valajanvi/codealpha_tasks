7fda9064bc669ef170ecefc1359b8cc6dbfc5be4
valajanvi <valajanvi@gmail.com>
2024-07-02 12:23:45 +0530

    first commit

---
+++ Task-2_calculator/js/index.js
@@ -0,0 +1,8 @@
+
+const buttons = document.querySelectorAll("#btn");
+const display = document.getElementById("display");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    display.value += button.value;
+  });
+});
